7b96861bbd4c36be66215a0c53630c847070530a
Ainara <ainarabliz@gmail.com>
2023-09-30 18:06:26 +0200

    nuevo code comentarioLinea

---
+++ Tarea01/src/main/comentarioLinea.java
@@ -0,0 +1,11 @@
+package main;
+
+public class comentarioLinea {
+
+	public static void main(String[] args) {
+		// System.out.println("Hola");
+		System.out.println("Adios");
+		System.out.println ("FIN DE SEMANA");
+	}
+
+}
